89d95e6bcb2a450dc465797754586b6631ffabc4
Farzad Jpt <farzad.jafarpour@gmail.com>
2022-02-26 23:11:11 +0330

    enhanced the userServices by changing the request body

--- src/services/userService.js
+++ src/services/userService.js
@@ -6,10 +6,5 @@ const { apiUrl } = endpoint;
 const apiEndpoint = apiUrl + "/users";
 
 export function register(user) {
-  return http.post(apiEndpoint, {
-    name: user.name,
-    lastName: user.lastName,
-    nationalCode: user.nationalCode,
-    password: user.password,
-  });
+  return http.post(apiEndpoint, user);
 }
