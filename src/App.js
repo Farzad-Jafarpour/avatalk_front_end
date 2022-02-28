9adf4b7f475254761b893f853dc226c5187b1fd7
Farzad Jpt <farzad.jafarpour@gmail.com>
2022-02-28 22:54:16 +0330

    Addet toastify to the App

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
+import { ToastContainer } from "react-toastify";
 import { Route, Routes } from "react-router-dom";
 import SignUp from "./components/signup";
 import NavBar from "./components/navbar";
 import Login from "./components/login";
+import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
 
 function App() {
   return (
     <React.Fragment>
+      <ToastContainer />
       <NavBar />
       <main className="container">
         <Routes className="content">
