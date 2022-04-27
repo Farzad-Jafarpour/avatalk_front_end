d1f1032c1f111cf5c28cf31ebd29c249344eaf68
Farzad Jpt <farzad.jafarpour@gmail.com>
2022-04-27 12:30:19 +0430

    made login component with material ui

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,51 +1,112 @@
-import React from "react";
-import Joi from "joi-browser";
-import Form from "../common/form";
-import auth from "./../services/authService";
+import * as React from "react";
+import { useForm } from "react-hook-form";
+import {
+  Avatar,
+  Button,
+  Container,
+  Link,
+  Paper,
+  Box,
+  Grid,
+  Typography,
+} from "@mui/material";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Copyright from "../common/copyright";
+import RenderInput from "../common/input";
+import auth from "../services/authService";
 
-class Login extends Form {
-  state = {
-    data: {
-      nationalCode: "",
-      password: "",
-    },
-    errors: {},
-  };
-  schema = {
-    nationalCode: Joi.string()
-      .min(10)
-      .max(10)
-      .required()
-      .label("National code"),
-    password: Joi.string().min(5).max(255).required().label("Password"),
-  };
+export default function Login() {
+  const { handleSubmit, control } = useForm();
 
-  doSubmit = async () => {
+  const onSubmit = async (data) => {
     try {
-      const { data } = this.state;
       await auth.login(data.nationalCode, data.password);
       window.location = "/";
     } catch (ex) {
       if (ex.response && ex.response.status === 400) {
-        const errors = { ...this.state.errors };
+        const errors = { ...data.errors };
         errors.nationalCode = ex.response.data;
-        this.setState({ errors });
+        data.errors = errors;
       }
     }
   };
 
-  render() {
-    return (
-      <React.Fragment>
-        <form onSubmit={this.handleSubmit}>
-          {this.renderInput("nationalCode", "National code")}
-          {this.renderInput("password", "Password", "password")}
-          {this.renderCheckOut()}
-          {this.renderButton("Log in")}
-        </form>
-      </React.Fragment>
-    );
-  }
+  return (
+    <Grid container component="main" sx={{ height: "80vh" }}>
+      <Grid
+        item
+        xs={false}
+        sm={4}
+        md={7}
+        sx={{
+          backgroundImage: "url(https://source.unsplash.com/random)",
+          backgroundRepeat: "no-repeat",
+          backgroundColor: (t) =>
+            t.palette.mode === "light"
+              ? t.palette.grey[50]
+              : t.palette.grey[900],
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      />
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Container component="main" maxWidth="xs">
+          <Box
+            sx={{
+              marginTop: 8,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Sign up
+            </Typography>
+            <Box component="div" sx={{ mt: 3 }}>
+              <Grid container spacing={2}>
+                <RenderInput
+                  rules={{
+                    required: "National code is required",
+                  }}
+                  fullWidth
+                  name="nationalCode"
+                  required
+                  label="National Code"
+                  control={control}
+                />
+                <RenderInput
+                  rules={{ required: "Password is required" }}
+                  fullWidth
+                  name="password"
+                  required
+                  label="Password"
+                  type="password"
+                  control={control}
+                />
+              </Grid>
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Sign Up
+              </Button>
+              <Grid container justifyContent="center">
+                <Grid item>
+                  <Link href="/signup" variant="body2">
+                    Don't have an account? Log in
+                  </Link>
+                </Grid>
+              </Grid>
+            </Box>
+          </Box>
+          <Copyright sx={{ mt: 5 }} companyName="Avatalk" />
+        </Container>
+      </form>
+    </Grid>
+  );
 }
-
-export default Login;
--- src/components/signup.jsx
+++ src/components/signup.jsx
@@ -19,7 +19,6 @@ export default function SignUp() {
 
   const onSubmit = async (data) => {
     try {
-      console.log(data);
       const response = await userService.register(data);
       auth.loginWithJwt(response.headers["x-auth-token"]);
       window.location = "/";
@@ -35,78 +34,96 @@ export default function SignUp() {
   // if (error) return <Error error={error} />;
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <Container component="main" maxWidth="xs">
-        <Box
-          sx={{
-            marginTop: 8,
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-          }}
-        >
-          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Sign up
-          </Typography>
-          <Box component="div" sx={{ mt: 3 }}>
-            <Grid container spacing={2}>
-              <RenderInput
-                rules={{ required: "First name is required" }}
-                sm={6}
-                name="name"
-                label="First Name"
-                control={control}
-              />
-              <RenderInput
-                rules={{ required: "Last name is required" }}
-                sm={6}
-                name="lastName"
-                required
-                label="Last Name"
-                control={control}
-              />
-              <RenderInput
-                rules={{
-                  required: "National code is required",
-                }}
-                fullWidth
-                name="nationalCode"
-                required
-                label="National Code"
-                control={control}
-              />
-              <RenderInput
-                rules={{ required: "Password is required" }}
-                fullWidth
-                name="password"
-                required
-                label="Password"
-                type="password"
-                control={control}
-              />
-            </Grid>
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Sign Up
-            </Button>
-            <Grid container justifyContent="center">
-              <Grid item>
-                <Link href="/login" variant="body2">
-                  Already have an account? Log in
-                </Link>
+    <Grid container component="main" sx={{ height: "80vh" }}>
+      <Grid
+        item
+        xs={false}
+        sm={4}
+        md={7}
+        sx={{
+          backgroundImage: "url(https://source.unsplash.com/random)",
+          backgroundRepeat: "no-repeat",
+          backgroundColor: (t) =>
+            t.palette.mode === "light"
+              ? t.palette.grey[50]
+              : t.palette.grey[900],
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      />
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Container component="main" maxWidth="xs">
+          <Box
+            sx={{
+              marginTop: 8,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Sign up
+            </Typography>
+            <Box component="div" sx={{ mt: 3 }}>
+              <Grid container spacing={2}>
+                <RenderInput
+                  rules={{ required: "First name is required" }}
+                  sm={6}
+                  name="name"
+                  label="First Name"
+                  control={control}
+                />
+                <RenderInput
+                  rules={{ required: "Last name is required" }}
+                  sm={6}
+                  name="lastName"
+                  required
+                  label="Last Name"
+                  control={control}
+                />
+                <RenderInput
+                  rules={{
+                    required: "National code is required",
+                  }}
+                  fullWidth
+                  name="nationalCode"
+                  required
+                  label="National Code"
+                  control={control}
+                />
+                <RenderInput
+                  rules={{ required: "Password is required" }}
+                  fullWidth
+                  name="password"
+                  required
+                  label="Password"
+                  type="password"
+                  control={control}
+                />
               </Grid>
-            </Grid>
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Sign Up
+              </Button>
+              <Grid container justifyContent="center">
+                <Grid item>
+                  <Link href="/login" variant="body2">
+                    Already have an account? Log in
+                  </Link>
+                </Grid>
+              </Grid>
+            </Box>
           </Box>
-        </Box>
-        <Copyright sx={{ mt: 5 }} companyName="Avatalk" />
-      </Container>
-    </form>
+          <Copyright sx={{ mt: 5 }} companyName="Avatalk" />
+        </Container>
+      </form>
+    </Grid>
   );
 }
