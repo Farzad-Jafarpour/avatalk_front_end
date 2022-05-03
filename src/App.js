a8286494cfc217f84c4b3067ace442115aa37422
Farzad Jpt <farzad.jafarpour@gmail.com>
2022-05-03 18:59:06 +0430

    made a user edition component

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import "react-toastify/dist/ReactToastify.css";
 import CssBaseline from "@mui/material/CssBaseline";
 import "./App.css";
 import AddUser from "./components/addUser";
+import EditUser from "./components/editUser";
 
 const theme = createTheme();
 
@@ -36,6 +37,7 @@ class App extends Component {
             <Routes className="content">
               <Route path="/signup" element={<SignUp />} />
               <Route path="/adduser" element={<AddUser />} />
+              <Route path="/edituser" element={<EditUser />} />
               <Route path="/users" element={<Users />} />
               <Route path="/classroom" element={<Classroom />} />
               <Route path="/classes" element={<ClassesNew />} />
