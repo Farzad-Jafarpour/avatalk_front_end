4a70d797809ac1c4ae2ccd96fb0ece304b0a69fd
Afshin Haghighatnia <afshin.hagigat@gmail.com>
2022-05-06 02:48:01 +0430

    pass validate only on Controller

--- src/common/input.jsx
+++ src/common/input.jsx
@@ -1,11 +1,18 @@
 import { useForm, Controller } from "react-hook-form";
 import { TextField, Grid } from "@mui/material";
 
-export default function RenderInput({ name, label, fullWidth, ...rest }) {
+export default function RenderInput({
+  name,
+  label,
+  fullWidth,
+  validate,
+  ...rest
+}) {
   return (
     <Controller
       {...rest}
       name={name}
+      validate
       render={({ field: { onChange, value }, fieldState: { error } }) => (
         <Grid item xs={12} {...rest}>
           <TextField
