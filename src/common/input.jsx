bcf9cfda23eeb6bba3f37c15b286cda12cccaf2f
Afshin Haghighatnia <afshin.hagigat@gmail.com>
2022-05-06 02:42:39 +0430

    fix fullWidth issue

--- src/common/input.jsx
+++ src/common/input.jsx
@@ -1,29 +1,7 @@
-// import React from "react";
-
-// const Input = ({ name, label, value, error, onChange, ...rest }) => {
-//   return (
-//     <div className="form-floating form-group mb-3">
-//       <input
-//         {...rest}
-//         name={name}
-//         value={value}
-//         onChange={onChange}
-//         id={name}
-//         placeholder={name}
-//         className="form-control"
-//       />
-//       <label htmlFor={name}>{label}</label>
-//       {error && <div className="alert alert-danger">{error}</div>}
-//     </div>
-//   );
-// };
-
-// export default Input;
-
 import { useForm, Controller } from "react-hook-form";
 import { TextField, Grid } from "@mui/material";
 
-export default function RenderInput({ name, label, ...rest }) {
+export default function RenderInput({ name, label, fullWidth, ...rest }) {
   return (
     <Controller
       {...rest}
@@ -37,6 +15,7 @@ export default function RenderInput({ name, label, ...rest }) {
             value={value}
             onChange={onChange}
             error={!!error}
+            fullWidth
             helperText={error ? error.message : null}
           />
         </Grid>
